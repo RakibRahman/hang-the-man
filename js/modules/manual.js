ed54a16e41251e862f13b82e03e924b436e6ef00
RakibRahman <md.rr.talukder@gmail.com>
2021-06-19 22:11:11 +0600

    how to play module added

--- js/modules/manual.js
+++ js/modules/manual.js
@@ -1,6 +1,37 @@
+import Menu from "./menu.js";
+import { sound } from "./../data/sound.js";
+
 const Manual = ((_) => {
+  const hangman = document.querySelector(".hangman");
   const init = (_) => {
-    console.log("Manual page");
+    render();
+    listeners();
+  };
+
+  const render = (_) => {
+    let markup = `
+    <h1 class="hangman__title">How To Play</h1>
+    <ul class="how">
+      <li><span>Playing</span> the game is pretty straightforward</li>
+      <li>All you need to do is<span> guess</span> the correct letters to make a word</li>
+      <li>You will have total <span>7</span> lives. </li>
+      <li>Lives will <span>decrease</span> each time you guess the wrong word</li>
+      <li>If you <span>lose</span>,your character will be <span>hanged</span></li>
+      <li>So, try your best to<span> win!</span></li>
+      <li><span>Good Luck</span></li>
+    </ul>
+    <button class="home button hangman__trigger"> <img src="./../../img/icons8-home-64.png" alt=""> </button>
+    `;
+
+    hangman.innerHTML = markup;
+  };
+  const listeners = (_) => {
+    const home = document.querySelector(".home");
+
+    home.addEventListener("click", () => {
+      Menu.init();
+      sound.click.play();
+    });
   };
   return {
     init,
